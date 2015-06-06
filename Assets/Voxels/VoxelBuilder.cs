f649d236895009018a363986546d359a8bacdf7b
Ben Wiklund <ben@daisyowl.com>
2015-06-06 10:47:07 -0700

    voxels

---
+++ Assets/Voxels/VoxelBuilder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoxelBuilder : MonoBehaviour {
+	public Transform voxelPrefab;
+
+	int xSize = 16, zSize = 16;
+
+	void Start () {
+		Build();
+	}
+
+	void Build () {
+		for(int x = 0; x < xSize; x++){
+			for(int z = 0; z < xSize; z++){
+				var pos = new Vector3(x, 0, z);
+				var obj = (Transform) Instantiate (voxelPrefab, pos, Quaternion.identity);
+				obj.SetParent(transform);
+
+				// foo data
+				var f = Random.Range(0.1f, 6f);
+				obj.GetComponent<VoxelCtrl>().SetValue(f);
+			}
+		}
+	}
+}

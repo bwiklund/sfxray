364430e613ada1f972277fa6c70580e60631b79d
Ben Wiklund <ben@daisyowl.com>
2015-06-06 14:16:04 -0700

    visual updates

---
+++ Assets/DatapointPinCtrl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DatapointPinCtrl : MonoBehaviour {
+
+	void Start () {
+		Randomize();
+	}
+
+	void Randomize(){
+		transform.position += new Vector3(0f, Random.Range (0f, -1f), 0f);
+	}
+}

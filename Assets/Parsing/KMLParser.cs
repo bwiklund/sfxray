cb51250dc8cd14c3a2336b0d8173e4e303521a69
Ben Wiklund <ben@daisyowl.com>
2015-06-06 14:03:03 -0700

    popos test

--- Assets/Parsing/KMLParser.cs
+++ Assets/Parsing/KMLParser.cs
@@ -1,21 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 public class KMLParser : DatapointLoader {
 	public override List<Datapoint> Load () {
 		return Parse ("/Users/ben/Downloads/POPOS/POPOS.kml");
 	}
-	
-	List<Datapoint> Parse(string filename) {
-		XmlDocument xdoc = new XmlDocument ();
-		try { xdoc.Load(filename); }
-		catch (System.IO.FileNotFoundException) {
-			Debug.Log("File not found: " + filename);
-		}
 
-		var placemarks = xdoc.FirstChild.NextSibling.FirstChild.ChildNodes;
-		return new List<Datapoint>();
+	List<Datapoint> Parse(string filename) {
+		// TODO: make a static util for converting latlon to game space for other uses
+		var topLeft = new Vector3(-122.5f, 0f, 37.8f);
+		var latLonScale = 0.25f;
+		var gameScale = 32f;
+		var flip = new Vector3(1f, 1f, -1f);
+
+		XNamespace ns = "http://earth.google.com/kml/2.2";
+		var xdoc = XDocument.Load("/Users/ben/Downloads/POPOS/POPOS.kml");
+		var placemarkQuery = xdoc.Root.Element(ns + "Document").Elements(ns + "Placemark");
+		var placemarks = placemarkQuery.Select( x => {
+			var coordinatesStr = x.Element(ns + "Point").Element (ns + "coordinates").Value;
+			var coordinatesArr = Regex.Split(coordinatesStr, ",");
+			var position = new Vector3(
+				float.Parse(coordinatesArr[0]),
+				float.Parse(coordinatesArr[2]),
+				float.Parse(coordinatesArr[1])
+			);
+			var transformedPosition = (position - topLeft) / latLonScale * gameScale;
+			transformedPosition.Scale(flip);
+			return new Datapoint {
+				position = transformedPosition
+			};
+		}).ToList();
+		Debug.Log ("Loading " + placemarks.Count + " kml placemarks");
+		return placemarks;
 	}
 }

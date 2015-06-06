d5d112ce5739c6aeff7e1736481eb0fc6e742e9f
Ben Wiklund <ben@daisyowl.com>
2015-06-06 15:49:54 -0700

    zipcode map

---
+++ Assets/Scenery/Zipcodes/ZipcodeMapCtrl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZipcodeMapCtrl : MonoBehaviour {
+	public Transform mapTransform;
+
+	void Start () {
+		Randomize();
+	}
+
+	void Randomize () {
+		foreach (Transform child in mapTransform) {
+			child.localScale = new Vector3(1f, 1f, Random.Range(1f, 2f));
+		}
+	}
+}

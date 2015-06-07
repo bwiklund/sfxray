95a8eb89a312f6842856163abce5a32a6260add1
Ben Wiklund <ben@daisyowl.com>
2015-06-07 11:09:15 -0700

    calibration helper

---
+++ Assets/Parsing/CalibrationService.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalibrationService : MonoBehaviour {
+	public static CalibrationService instance;
+	
+	public Transform leftCalibrationPoint, rightCalibrationPoint;
+
+	Vector3 transformOffset;
+	float transformScale;
+
+	void Awake () {
+		instance = this;
+		UpdateCalibration();
+	}
+
+	void UpdateCalibration () {
+		// we need to figure out a translation and scaling that will put these in the right space:
+		// our calibration points for convenience
+		// LEFT: -122.44642, 37.79275
+		// RIGHT: -122.42347, 37.79572
+		var leftCalibrationPointLatLon = new Vector3(-122.44642f, 0f, 37.79275f);
+		var rightCalibrationPointLatLon = new Vector3(-122.42347f, 0f, 37.79572f);
+		
+		var leftCalibrationPointGameSpace = leftCalibrationPoint.transform.position;
+		var rightCalibrationPointGameSpace = rightCalibrationPoint.transform.position;
+		
+		leftCalibrationPointGameSpace.Scale(new Vector3(1f, 0f, 1f));
+		rightCalibrationPointGameSpace.Scale(new Vector3(1f, 0f, 1f));
+		
+		var worldDistance = (leftCalibrationPointLatLon - rightCalibrationPointLatLon).magnitude;
+		var gameDistance = (leftCalibrationPointGameSpace - rightCalibrationPointGameSpace).magnitude;
+		
+		transformScale = gameDistance / worldDistance;
+		transformOffset = leftCalibrationPointGameSpace - (leftCalibrationPointLatLon * transformScale);
+		
+		Debug.Log(transformOffset);
+		Debug.Log(transformScale);
+
+		var flip = new Vector3(1f, 1f, -1f);
+	}
+
+	public Vector3 LatLonToGameSpace (Vector3 position) {
+		return (position * transformScale) + transformOffset;
+	}
+}
--- Assets/Parsing/KMLParser.cs
+++ Assets/Parsing/KMLParser.cs
@@ -6,40 +6,13 @@ using System.Xml.Linq;
 using System.Text.RegularExpressions;
 
 public class KMLParser : DatapointLoader {
-	public Transform leftCalibrationPoint, rightCalibrationPoint;
 
 	public override List<Datapoint> Load () {
 		return Parse ("/Users/ben/Downloads/POPOS/POPOS.kml");
 	}
 
 	List<Datapoint> Parse(string filename) {
-		// we need to figure out a translation and scaling that will put these in the right space:
-		// our calibration points for convenience
-		// LEFT: -122.44642, 37.79275
-		// RIGHT: -122.42347, 37.79572
-		var leftCalibrationPointLatLon = new Vector3(-122.44642f, 0f, 37.79275f);
-		var rightCalibrationPointLatLon = new Vector3(-122.42347f, 0f, 37.79572f);
-
-		var leftCalibrationPointGameSpace = leftCalibrationPoint.transform.position;
-		var rightCalibrationPointGameSpace = rightCalibrationPoint.transform.position;
-		
-		leftCalibrationPointGameSpace.Scale(new Vector3(1f, 0f, 1f));
-		rightCalibrationPointGameSpace.Scale(new Vector3(1f, 0f, 1f));
-
-		var worldDistance = (leftCalibrationPointLatLon - rightCalibrationPointLatLon).magnitude;
-		var gameDistance = (leftCalibrationPointGameSpace - rightCalibrationPointGameSpace).magnitude;
-
-		var transformScale = gameDistance / worldDistance;
-		var transformOffset = leftCalibrationPointGameSpace - (leftCalibrationPointLatLon * transformScale);
-		
-		Debug.Log(transformOffset);
-		Debug.Log(transformScale);
-
-		// TODO: make a static util for converting latlon to game space for other uses
-//		var topLeft = new Vector3(-122.554379f, 0f, 37.814353f);
-//		var latLonScale = 0.25f;
-//		var gameScale = 32f;
-		var flip = new Vector3(1f, 1f, -1f);
+		//!
 
 		XNamespace ns = "http://earth.google.com/kml/2.2";
 		var xdoc = XDocument.Load("/Users/ben/Downloads/POPOS/POPOS.kml");
@@ -52,10 +25,9 @@ public class KMLParser : DatapointLoader {
 				float.Parse(coordinatesArr[2]),
 				float.Parse(coordinatesArr[1])
 			);
-			var transformedPosition = (position * transformScale) + transformOffset;
 //			transformedPosition.Scale(flip);
 			return new Datapoint {
-				position = transformedPosition
+				position = CalibrationService.instance.LatLonToGameSpace(position)
 			};
 		}).ToList();
 		Debug.Log ("Loading " + placemarks.Count + " kml placemarks");

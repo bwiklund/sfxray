272ae3f26091737fb62a006df4b4c2d2862a15dc
Ben Wiklund <ben@daisyowl.com>
2015-07-01 22:11:49 -0700

    building p sys

--- Assets/ObjParticleBehaviour.cs
+++ Assets/ObjParticleBehaviour.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ObjParticleBehaviour : MonoBehaviour {
   void Start() {
@@ -8,8 +10,18 @@ public class ObjParticleBehaviour : MonoBehaviour {
     };
     var points = loader.Load();
 
+    var pSystem = GetComponent<ParticleSystem>();
+    var particles = new List<ParticleSystem.Particle>();
     foreach (var point in points) {
-      
+      var particle = new ParticleSystem.Particle() {
+        color = Color.white,
+        size = 0.01f,
+        startLifetime = 5000f,
+        position = point * 0.01f
+      };
+      particles.Add(particle);
     }
+    pSystem.SetParticles(particles.ToArray(), particles.Count);
+ 
   }
 }

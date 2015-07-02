8b94d421ed81ae57375fec60842a2c95594b48ea
Ben Wiklund <ben@daisyowl.com>
2015-07-01 21:04:03 -0700

    parsing obj

---
+++ Assets/ObjParticleBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjParticleBehaviour : MonoBehaviour {
+  void Start() {
+    var loader = new ObjToParticles() {
+      filepath = "/particles/Scaled/Scaled.obj"
+    };
+    var points = loader.Load();
+
+    foreach (var point in points) {
+      
+    }
+  }
+}
---
+++ Assets/ObjToParticles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class ObjToParticles {
+  public string filepath;
+
+  public List<Vector3> Load() {
+    StreamReader reader = new StreamReader(filepath);
+    var points = new List<Vector3>();
+
+    while (reader.Peek() != -1) {
+      var line = reader.ReadLine();
+      var parts = line.Split(' ');
+      if (parts[0] == "v") {
+        var point = new Vector3(
+          float.Parse(parts[1]),
+          float.Parse(parts[2]),
+          float.Parse(parts[3])
+        );
+        points.Add(point);
+      }
+    }
+
+    Debug.Log("Loaded points: " + points.Count);
+
+    return points;
+  }
+}

36ae39f63cae9f9811f1b0160452840426da27e7
Ben Wiklund <ben@daisyowl.com>
2015-07-08 22:18:05 -0700

    merge

---
+++ Assets/Spin.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	void Update () {
+    transform.rotation = Quaternion.AngleAxis(Time.time, Vector3.up);
+	}
+}
